4788b9ad02df89ea80d2caaf8777b2e62e1ec343
vvr3ddy <vvr-rockstar@bmsit.in>
2021-07-07 17:50:20 +0530

    :octocat: Controllers: Enable Cross Origin Policy
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/main/java/org/wolf/controller/CollegeController.java
+++ src/main/java/org/wolf/controller/CollegeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.wolf.service.ICollegeService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/college")
 public class CollegeController {
--- src/main/java/org/wolf/controller/StudentController.java
+++ src/main/java/org/wolf/controller/StudentController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.wolf.service.IStudentService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/student")
 public class StudentController {
